3cb15c11f242d6743b2389def0bfa9e4613cee4c
Eric Le <eriiicle@outlook.com>
2026-01-30 05:39:07 +0100

    refactor(utils): centralize quickpick command handling

--- src/utils/picker-utils.ts
+++ src/utils/picker-utils.ts
@@ -1,81 +1,92 @@
 import * as vscode from 'vscode'
 
+let activePicker: vscode.QuickPick<any> | undefined
+let globalPickerCommands: vscode.Disposable | undefined
+
+const CONTEXT_KEY = 'aiStackerPickerVisible'
+const CMD_TOGGLE = 'aiContextStacker.internalToggle'
+const CMD_SELECT_ALL = 'aiContextStacker.internalSelectAll'
+const CMD_NEXT = 'aiContextStacker.internalNext'
+const CMD_PREV = 'aiContextStacker.internalPrev'
+
 export function attachPickerToggle<T extends vscode.QuickPickItem>(picker: vscode.QuickPick<T>): vscode.Disposable {
-  const contextKey = 'aiStackerPickerVisible'
-  const toggleCommandId = 'aiContextStacker.internalToggle'
-  const selectAllCommandId = 'aiContextStacker.internalSelectAll'
-  const nextCommandId = 'aiContextStacker.internalNext'
-  const prevCommandId = 'aiContextStacker.internalPrev'
+  activePicker = picker
+  void vscode.commands.executeCommand('setContext', CONTEXT_KEY, true)
 
-  void vscode.commands.executeCommand('setContext', contextKey, true)
-
-  const toggleCommand = vscode.commands.registerCommand(toggleCommandId, () => {
-    const activeItems = picker.activeItems
-    if (activeItems.length === 0) return
-
-    const selectedSet = new Set(picker.selectedItems)
-    const activeSet = new Set(activeItems)
-
-    for (const item of activeSet) {
-      if (selectedSet.has(item)) {
-        selectedSet.delete(item)
-      } else {
-        selectedSet.add(item)
-      }
-    }
-
-    picker.selectedItems = Array.from(selectedSet)
-  })
-
-  const selectAllCommand = vscode.commands.registerCommand(selectAllCommandId, () => {
-    const allItems = picker.items.filter((item) => item.kind !== vscode.QuickPickItemKind.Separator)
-    const query = picker.value.trim().toLowerCase()
-
-    const visibleItems = query
-      ? allItems.filter((item) => {
-          if (item.alwaysShow) return true
-          if (picker.activeItems.includes(item as unknown as T)) return true
-
-          const label = item.label.toLowerCase()
-          const desc = (item.description || '').toLowerCase()
-          return label.includes(query) || desc.includes(query)
-        })
-      : allItems
-
-    if (visibleItems.length === 0) return
-
-    const selectedSet = new Set(picker.selectedItems)
-    const allVisibleSelected = visibleItems.every((item) => selectedSet.has(item as unknown as T))
-
-    if (allVisibleSelected) {
-      const visibleSet = new Set(visibleItems)
-      picker.selectedItems = picker.selectedItems.filter((i) => !visibleSet.has(i as unknown as T))
-    } else {
-      const toAdd = visibleItems.filter((item) => !selectedSet.has(item as unknown as T))
-      picker.selectedItems = [...picker.selectedItems, ...toAdd] as T[]
-    }
-  })
-
-  const nextCommand = vscode.commands.registerCommand(nextCommandId, () => {
-    moveActiveItem(picker, 1)
-  })
-
-  const prevCommand = vscode.commands.registerCommand(prevCommandId, () => {
-    moveActiveItem(picker, -1)
-  })
+  if (!globalPickerCommands) {
+    globalPickerCommands = vscode.Disposable.from(
+      vscode.commands.registerCommand(CMD_TOGGLE, () => {
+        if (activePicker) handleToggle(activePicker)
+      }),
+      vscode.commands.registerCommand(CMD_SELECT_ALL, () => {
+        if (activePicker) handleSelectAll(activePicker)
+      }),
+      vscode.commands.registerCommand(CMD_NEXT, () => {
+        if (activePicker) moveActiveItem(activePicker, 1)
+      }),
+      vscode.commands.registerCommand(CMD_PREV, () => {
+        if (activePicker) moveActiveItem(activePicker, -1)
+      }),
+    )
+  }
 
   const cleanup = () => {
-    void vscode.commands.executeCommand('setContext', contextKey, false)
-    toggleCommand.dispose()
-    selectAllCommand.dispose()
-    nextCommand.dispose()
-    prevCommand.dispose()
-    hideListener.dispose()
+    if (activePicker === picker) {
+      activePicker = undefined
+      void vscode.commands.executeCommand('setContext', CONTEXT_KEY, false)
+    }
   }
 
   const hideListener = picker.onDidHide(cleanup)
 
-  return vscode.Disposable.from(toggleCommand, selectAllCommand, nextCommand, prevCommand, hideListener)
+  return vscode.Disposable.from(hideListener)
+}
+
+function handleToggle<T extends vscode.QuickPickItem>(picker: vscode.QuickPick<T>): void {
+  const activeItems = picker.activeItems
+  if (activeItems.length === 0) return
+
+  const selectedSet = new Set(picker.selectedItems)
+  const activeSet = new Set(activeItems)
+
+  for (const item of activeSet) {
+    if (selectedSet.has(item)) {
+      selectedSet.delete(item)
+    } else {
+      selectedSet.add(item)
+    }
+  }
+
+  picker.selectedItems = Array.from(selectedSet)
+}
+
+function handleSelectAll<T extends vscode.QuickPickItem>(picker: vscode.QuickPick<T>): void {
+  const allItems = picker.items.filter((item) => item.kind !== vscode.QuickPickItemKind.Separator)
+  const query = picker.value.trim().toLowerCase()
+
+  const visibleItems = query
+    ? allItems.filter((item) => {
+        if (item.alwaysShow) return true
+        if (picker.activeItems.includes(item as unknown as T)) return true
+
+        const label = item.label.toLowerCase()
+        const desc = (item.description || '').toLowerCase()
+        return label.includes(query) || desc.includes(query)
+      })
+    : allItems
+
+  if (visibleItems.length === 0) return
+
+  const selectedSet = new Set(picker.selectedItems)
+  const allVisibleSelected = visibleItems.every((item) => selectedSet.has(item as unknown as T))
+
+  if (allVisibleSelected) {
+    const visibleSet = new Set(visibleItems)
+    picker.selectedItems = picker.selectedItems.filter((i) => !visibleSet.has(i as unknown as T))
+  } else {
+    const toAdd = visibleItems.filter((item) => !selectedSet.has(item as unknown as T))
+    picker.selectedItems = [...picker.selectedItems, ...toAdd] as T[]
+  }
 }
 
 function moveActiveItem<T extends vscode.QuickPickItem>(picker: vscode.QuickPick<T>, direction: number): void {
